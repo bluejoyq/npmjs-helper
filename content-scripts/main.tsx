dec49258547a790b2f5d98a79065e6dfdf52c046
bluejoyq <bluejoy32@gmail.com>
2023-11-28 00:06:48 +0900

    init

---
+++ content-scripts/main.tsx
@@ -0,0 +1,23 @@
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
+
+const getNode = () => {
+  const h3Elements = document.querySelectorAll("h3"); // 모든 <h3> 요소를 선택합니다.
+
+  for (const h3Element of h3Elements) {
+    if (h3Element.textContent === "Install") {
+      const nextSibling = h3Element.nextElementSibling;
+      if (nextSibling) {
+        return nextSibling;
+      }
+    }
+  }
+
+
+}
+const node = getNode()!
+node.parentElement?.insertBefore(node.cloneNode(true),node);
+const root = createRoot(node);
+
+root.render(<App />)
+
