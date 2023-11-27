86af58834727503bfe6275b3040e251212a730c0
bluejoyq <bluejoy32@gmail.com>
2023-11-28 00:11:35 +0900

    feat: add emotion to style

--- content-scripts/App.tsx
+++ content-scripts/App.tsx
@@ -1,3 +1,14 @@
+import { css } from "@emotion/react";
 export const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1
+        css={css`
+          color: red;
+        `}
+      >
+        a
+      </h1>
+    </div>
+  );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { crx } from '@crxjs/vite-plugin'
-import manifest from './manifest.json'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { crx } from "@crxjs/vite-plugin";
+import manifest from "./manifest.json";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    react({
+      jsxImportSource: "@emotion/react",
+      babel: {
+        plugins: ["@emotion/babel-plugin"],
+      },
+    }),
     crx({ manifest }),
   ],
-})
+});
